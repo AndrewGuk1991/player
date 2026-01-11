06daaaf5522435c43ea87162e23865b3a194f7ea
andrew_guk <gukandrey91@gmail.com>
2026-01-11 16:03:43 +0300

    remove title

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -13,7 +13,6 @@ export function MainPage() {
     return (
         <>
             <div style={{display: 'flex', gap: '30px'}}>
-                <h1>Musicfun Player</h1>
                 <TrackList
                     selectedTrackId={trackId}
                     onTrackSelect={handleTrackItem}
